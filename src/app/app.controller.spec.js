b62c801c95c9c3fb3ddeb50ed6d71917a388e896
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-01 18:19:47 +0000

    Deploy eustatos/angularjs-starter to github.com/eustatos/angularjs-starter.git:gh-pages

--- src/app/app.controller.spec.js
+++
@@ -1,8 +0,0 @@
-import AppController from './app.controller';
-
-describe('AppController', () => {
-    it('must be create', () => {
-        const appController = new AppController();
-        expect(appController).toBeDefined();
-    })
-})
